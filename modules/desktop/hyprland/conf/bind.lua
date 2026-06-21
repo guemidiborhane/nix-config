guemidiborhane/dotfiles

:recycle: hyprland: introduce prefix dispatch DSL
Decompose the monolithic helpers.lua into three focused modules:
- vars.lua: pure static data (mods, apps, messaging_clients). Eliminates
the
  workspace.lua anti-pattern of mutating a shared required module's
table at
  load time (h.messaging_clients = {...}).
- dsp.lua: prefix-based command dispatch table. Resolves
"prefix:payload"
  strings to concrete exec actions at bind time. Built-in handlers: sh:,
  app:, term:, popup:. Extensible via dispatchers:add() for call-site
prefixes
  (vic:, noc: in bind.lua). M.exec/M.cmd both thread opts through to
handlers,
  preserving slice/app_id context.
- helpers.lua: utility functions only — bind, define_submap,
get_hostname,
  get_active_or_special_workspace, notify. bind() auto-resolves string
actions
  through dsp, making all keybinding declarations fully declarative.

modules/desktop/hyprland/conf/bind.lua
@@ -1,4 +1,6 @@
 local h = require("lua.helpers")
+local v = require("lua.vars")
+local d = require("lua.dsp")
 
 hl.config({
   binds = {
@@ -11,67 +13,54 @@ hl.config({
 })
 
 local bind = h.bind
-
-local Meta = h.mods.Meta
-local Shift = h.mods.Shift
-local Control = h.mods.Control
-local Alt = h.mods.Alt
-
-local exec_cmd = hl.dsp.exec_cmd
+local Meta, Control, Shift, Alt = v.mods.Meta, v.mods.Control, v.mods.Shift, v.mods.Alt
 
 bind("W", { Meta, Control }, hl.dsp.group.toggle())
 
-local function drun(command) return h.drun:exec_cmd(command) end
-bind("Return", Meta, h.terminal:exec("s"))
-bind("E", { Meta, Control }, drun("thunar"))
-bind("Escape", Control, drun("missioncenter"))
-bind("A", Meta, h.shell:exec("sshkey"))
+bind("Return", Meta, "term:s")
+bind("E", { Meta, Control }, "app:thunar")
+bind("Escape", Control, "app:missioncenter")
+bind("A", Meta, "sh:sshkey")
 
-local function exec(cmd)
-  if cmd:find("^%$") ~= nil then return exec_cmd(cmd:sub(2)) end
-  return drun(cmd)
+for _, app in next, v.apps do
+  bind(app.key, Meta, app.main)
+  if app.alt then bind(app.key, { Meta, Alt }, app.alt) end
 end
 
-for _, app in next, h.apps do
-  bind(app.key, Meta, exec(app.cmd))
-  if app.alt then bind(app.key, { Meta, Alt }, exec(app.alt)) end
-end
+bind("C", Meta, "hyprpicker -a")
 
-bind("C", Meta, exec_cmd("hyprpicker -a"))
+bind("Print", nil, "capture -m region")
+bind("Print", Shift, "capture")
 
-bind("Print", nil, exec_cmd("capture -m region"))
-bind("Print", Shift, exec_cmd("capture"))
+bind("X", { Meta, Control }, "hyprctl kill")
+bind("R", { Meta, Control, Shift }, "hypres restore")
+bind("R", { Meta, Shift }, "hyprctl reload")
 
-bind("X", { Meta, Control }, exec_cmd("hyprctl kill"))
-bind("R", { Meta, Control, Shift }, exec_cmd("hypres restore"))
-bind("R", { Meta, Shift }, exec_cmd("hyprctl reload"))
-
-local function popup_terminal(cmd) return exec_cmd("terminal-popup " .. cmd) end
-bind("M", Meta, popup_terminal("wiremix --tab output"))
-bind("B", { Meta, Shift }, popup_terminal("bluetui"))
-bind("W", { Meta, Shift }, popup_terminal("wlctl"))
+bind("M", Meta, "popup:wiremix --tab output")
+bind("B", { Meta, Shift }, "popup:bluetui")
+bind("W", { Meta, Shift }, "popup:wlctl")
 
 hl.layer_rule({ match = { namespace = "vicinae" }, blur = true, ignore_alpha = 0 })
-local function vicinae(deep_link) return exec_cmd("vicinae vicinae://" .. deep_link) end
-bind("Space", Meta, vicinae("toggle"))
-bind("Grave", Meta, vicinae("launch/wm/switch-windows"))
-bind("Space", { Control, Shift }, vicinae("launch/core/search-emojis"))
-bind("H", { Control, Alt }, vicinae("launch/clipboard/history?toggle=true"))
+d.dispatchers:add('vic', function(cmd) return "vicinae vicinae://" .. cmd end)
+bind("Space", Meta, "vic:toggle")
+bind("Grave", Meta, "vic:launch/wm/switch-windows")
+bind("Space", { Control, Shift }, "vic:launch/core/search-emojis")
+bind("H", { Control, Alt }, "vic:launch/clipboard/history")
 
-local function noctalia(ipc) return exec_cmd("noctalia-shell ipc call " .. ipc) end
-bind(0, Meta, noctalia("sessionMenu toggle"))
-bind("Z", Alt, noctalia("notifications toggleHistory"))
+d.dispatchers:add('noc', function(cmd) return "noctalia-shell ipc call " .. cmd end)
+bind(0, Meta, "noc:sessionMenu toggle")
+bind("Z", Alt, "noc:notifications toggleHistory")
 bind("L", { Alt, Shift }, function()
-  hl.dispatch(noctalia("media pause"))
-  hl.dispatch(noctalia("lockScreen lock"))
+  hl.dispatch(d.exec("noc:media pause"))
+  hl.dispatch(d.exec("noc:lockScreen lock"))
   hl.timer(function() hl.dispatch(hl.dsp.dpms({ action = "disable" })) end, { timeout = 500, type = "oneshot" })
 end, { release = true })
 h.define_submap("W", Meta, "wallpaper", function()
-  bind("C", nil, noctalia("wallpaper toggle"))
+  bind("C", nil, "noc:wallpaper toggle")
   bind("R", nil, function()
     local monitors = hl.get_monitors()
     for _, monitor in ipairs(monitors) do
-      hl.dispatch(noctalia("wallpaper random " .. monitor.name))
+      hl.dispatch(d.exec("noc:wallpaper random " .. monitor.name))
     end
   end)
 end)
@@ -89,12 +78,12 @@ local special_keys = {
   AudioPlay = { ipc = "media playPause" },
 }
 for key, entry in next, special_keys do
-  bind("XF86" .. key, nil, noctalia(entry.ipc), { locked = true, repeating = entry.repeating or false })
+  bind("XF86" .. key, nil, "noc:" .. entry.ipc, { locked = true, repeating = entry.repeating or false })
 end
-bind("XF86AudioNext", Shift, noctalia("media previous"), { locked = true })
+bind("XF86AudioNext", Shift, "noc:media previous", { locked = true })
 
 h.define_submap("V", Meta, "vpn", function()
-  bind("c", nil, exec_cmd("vpn connect"))
-  bind("d", nil, exec_cmd("vpn disconnect"))
-  bind("r", nil, exec_cmd("vpn reconnect"))
+  bind("c", nil, "sh:vpn connect")
+  bind("d", nil, "sh:vpn disconnect")
+  bind("r", nil, "sh:vpn reconnect")
 end)

modules/desktop/hyprland/conf/workspace.lua
@@ -1,40 +1,31 @@
 local h = require("lua.helpers")
+local v = require("lua.vars")
+local d = require("lua.dsp")
 
-h.messaging_clients = {
-  { class = "thunderbird" },
-  { class = "org.telegram.desktop" },
-  { class = "signal" },
-  { class = "vesktop" },
-}
+local bind = h.bind
+local Meta, Control, Shift, Alt = v.mods.Meta, v.mods.Control, v.mods.Shift, v.mods.Alt
 
-local Meta = h.mods.Meta
-local Shift = h.mods.Shift
-local Control = h.mods.Control
-local Alt = h.mods.Alt
+d.dispatchers:add("tmux", function(session_name)
+  local cmd = string.format("tmux new-session -As %q", session_name)
+  return d.cmd("term:" .. cmd, session_name .. "-session")
+end)
 
-local function open_tmux_session_cmd(name)
-  local cmd = string.format("tmux new-session -As %q", name)
-  return h.terminal:cmd(cmd, name .. "-session")
-end
-
-local function drun_cmd(cmd) return h.drun:cmd(cmd) end
 local workspaces = {
-  [1] = { on_created_empty = drun_cmd(h.apps.browser.cmd) },
-  [2] = { on_created_empty = drun_cmd("helium") },
+  [1] = { on_created_empty = v.apps.browser.main },
+  [2] = { on_created_empty = "app:helium" },
   [3] = {
-    on_created_empty = open_tmux_session_cmd("monitors"),
-    clients = { { class = "monitors-session" } }
+    on_created_empty = "tmux:monitors",
+    clients = {
+      { class = "monitors-session" }
+    }
   },
 
   -- named/special
-  workshop = { key = "G", on_created_empty = open_tmux_session_cmd("workshop") },
-  messaging = {
-    key = "D",
-    clients = h.messaging_clients,
-  },
+  workshop = { key = "G", on_created_empty = "tmux:workshop" },
+  messaging = { key = "D", clients = v.messaging_clients, },
   music = {
     key = "S",
-    on_created_empty = drun_cmd("env -u DISPLAY spotify"),
+    on_created_empty = "app:env -u DISPLAY spotify",
     clients = {
       { class = "^(spotify)$" },
     },
@@ -66,22 +57,23 @@ local function smart_focus(workspace, close_all)
     hl.dispatch(dispatcher)
   end
 end
+
 local function workspace_rules(name, mod, i)
   local workspace = string.gsub(tostring(name), special_prefix, "")
   local opts = workspaces[tonumber(workspace) or workspace]
   local key = (opts and opts.key) or i or workspace
 
-  h.bind(key, mod, smart_focus(name, true))
-  h.bind(key, { mod, Alt }, smart_focus(name, false))
-  h.bind(key, { mod, Shift }, hl.dsp.window.move({ workspace = name, follow = true }))
-  h.bind(key, { mod, Control }, hl.dsp.window.move({ workspace = name, follow = false }))
+  bind(key, mod, smart_focus(name, true))
+  bind(key, { mod, Alt }, smart_focus(name, false))
+  bind(key, { mod, Shift }, hl.dsp.window.move({ workspace = name, follow = true }))
+  bind(key, { mod, Control }, hl.dsp.window.move({ workspace = name, follow = false }))
 
   if not opts then return end
 
   if opts.on_created_empty then
     hl.workspace_rule({
       workspace = tostring(name),
-      on_created_empty = opts.on_created_empty,
+      on_created_empty = d.cmd(opts.on_created_empty),
     })
   end
 
@@ -113,7 +105,7 @@ for name, _ in next, workspaces do
   end
 end
 
-h.bind("T", { Meta, Shift }, function()
+bind("T", { Meta, Shift }, function()
   local layouts = { "dwindle", "master", "scrolling", "monocle" }
 
   local active = h.get_active_or_special_workspace()
@@ -135,10 +127,10 @@ h.bind("T", { Meta, Shift }, function()
   hl.notification.create({ timeout = 2000, text = next_layout })
 end)
 
-h.bind("Tab", Meta, hl.dsp.focus({ workspace = "m+1" }))
-h.bind("Tab", { Meta, Shift }, hl.dsp.focus({ workspace = "m-1" }))
-h.bind("mouse_up", Meta, hl.dsp.focus({ workspace = "m-1" }))
-h.bind("mouse_down", Meta, hl.dsp.focus({ workspace = "m+1" }))
+bind("Tab", Meta, hl.dsp.focus({ workspace = "m+1" }))
+bind("Tab", { Meta, Shift }, hl.dsp.focus({ workspace = "m-1" }))
+bind("mouse_up", Meta, hl.dsp.focus({ workspace = "m-1" }))
+bind("mouse_down", Meta, hl.dsp.focus({ workspace = "m+1" }))
 
 hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
 hl.gesture({ fingers = 3, direction = "vertical", action = "special", workspace_name = "workshop" })

guemidiborhane/nix-config

modules/desktop/hyprland/conf/bind.lua
@@ -1,4 +1,6 @@
 local h = require("lua.helpers")
+local v = require("lua.vars")
+local d = require("lua.dsp")
 
 hl.config({
   binds = {
@@ -11,67 +13,54 @@ hl.config({
 })
 
 local bind = h.bind
-
-local Meta = h.mods.Meta
-local Shift = h.mods.Shift
-local Control = h.mods.Control
-local Alt = h.mods.Alt
-
-local exec_cmd = hl.dsp.exec_cmd
+local Meta, Control, Shift, Alt = v.mods.Meta, v.mods.Control, v.mods.Shift, v.mods.Alt
 
 bind("W", { Meta, Control }, hl.dsp.group.toggle())
 
-local function drun(command) return h.drun:exec_cmd(command) end
-bind("Return", Meta, h.terminal:exec("s"))
-bind("E", { Meta, Control }, drun("thunar"))
-bind("Escape", Control, drun("missioncenter"))
-bind("A", Meta, h.shell:exec("sshkey"))
+bind("Return", Meta, "term:s")
+bind("E", { Meta, Control }, "app:thunar")
+bind("Escape", Control, "app:missioncenter")
+bind("A", Meta, "sh:sshkey")
 
-local function exec(cmd)
-  if cmd:find("^%$") ~= nil then return exec_cmd(cmd:sub(2)) end
-  return drun(cmd)
+for _, app in next, v.apps do
+  bind(app.key, Meta, app.main)
+  if app.alt then bind(app.key, { Meta, Alt }, app.alt) end
 end
 
-for _, app in next, h.apps do
-  bind(app.key, Meta, exec(app.cmd))
-  if app.alt then bind(app.key, { Meta, Alt }, exec(app.alt)) end
-end
+bind("C", Meta, "hyprpicker -a")
 
-bind("C", Meta, exec_cmd("hyprpicker -a"))
+bind("Print", nil, "capture -m region")
+bind("Print", Shift, "capture")
 
-bind("Print", nil, exec_cmd("capture -m region"))
-bind("Print", Shift, exec_cmd("capture"))
+bind("X", { Meta, Control }, "hyprctl kill")
+bind("R", { Meta, Control, Shift }, "hypres restore")
+bind("R", { Meta, Shift }, "hyprctl reload")
 
-bind("X", { Meta, Control }, exec_cmd("hyprctl kill"))
-bind("R", { Meta, Control, Shift }, exec_cmd("hypres restore"))
-bind("R", { Meta, Shift }, exec_cmd("hyprctl reload"))
-
-local function popup_terminal(cmd) return exec_cmd("terminal-popup " .. cmd) end
-bind("M", Meta, popup_terminal("wiremix --tab output"))
-bind("B", { Meta, Shift }, popup_terminal("bluetui"))
-bind("W", { Meta, Shift }, popup_terminal("wlctl"))
+bind("M", Meta, "popup:wiremix --tab output")
+bind("B", { Meta, Shift }, "popup:bluetui")
+bind("W", { Meta, Shift }, "popup:wlctl")
 
 hl.layer_rule({ match = { namespace = "vicinae" }, blur = true, ignore_alpha = 0 })
-local function vicinae(deep_link) return exec_cmd("vicinae vicinae://" .. deep_link) end
-bind("Space", Meta, vicinae("toggle"))
-bind("Grave", Meta, vicinae("launch/wm/switch-windows"))
-bind("Space", { Control, Shift }, vicinae("launch/core/search-emojis"))
-bind("H", { Control, Alt }, vicinae("launch/clipboard/history?toggle=true"))
+d.dispatchers:add('vic', function(cmd) return "vicinae vicinae://" .. cmd end)
+bind("Space", Meta, "vic:toggle")
+bind("Grave", Meta, "vic:launch/wm/switch-windows")
+bind("Space", { Control, Shift }, "vic:launch/core/search-emojis")
+bind("H", { Control, Alt }, "vic:launch/clipboard/history")
 
-local function noctalia(ipc) return exec_cmd("noctalia-shell ipc call " .. ipc) end
-bind(0, Meta, noctalia("sessionMenu toggle"))
-bind("Z", Alt, noctalia("notifications toggleHistory"))
+d.dispatchers:add('noc', function(cmd) return "noctalia-shell ipc call " .. cmd end)
+bind(0, Meta, "noc:sessionMenu toggle")
+bind("Z", Alt, "noc:notifications toggleHistory")
 bind("L", { Alt, Shift }, function()
-  hl.dispatch(noctalia("media pause"))
-  hl.dispatch(noctalia("lockScreen lock"))
+  hl.dispatch(h.exec("noc:media pause"))
+  hl.dispatch(h.exec("noc:lockScreen lock"))
   hl.timer(function() hl.dispatch(hl.dsp.dpms({ action = "disable" })) end, { timeout = 500, type = "oneshot" })
 end, { release = true })
 h.define_submap("W", Meta, "wallpaper", function()
-  bind("C", nil, noctalia("wallpaper toggle"))
+  bind("C", nil, "noc:wallpaper toggle")
   bind("R", nil, function()
     local monitors = hl.get_monitors()
     for _, monitor in ipairs(monitors) do
-      hl.dispatch(noctalia("wallpaper random " .. monitor.name))
+      h.exec("noc:wallpaper random " .. monitor.name)
     end
   end)
 end)
@@ -89,12 +78,12 @@ local special_keys = {
   AudioPlay = { ipc = "media playPause" },
 }
 for key, entry in next, special_keys do
-  bind("XF86" .. key, nil, noctalia(entry.ipc), { locked = true, repeating = entry.repeating or false })
+  bind("XF86" .. key, nil, "noc:" .. entry.ipc, { locked = true, repeating = entry.repeating or false })
 end
-bind("XF86AudioNext", Shift, noctalia("media previous"), { locked = true })
+bind("XF86AudioNext", Shift, "noc:media previous", { locked = true })
 
 h.define_submap("V", Meta, "vpn", function()
-  bind("c", nil, exec_cmd("vpn connect"))
-  bind("d", nil, exec_cmd("vpn disconnect"))
-  bind("r", nil, exec_cmd("vpn reconnect"))
+  bind("c", nil, "sh:vpn connect")
+  bind("d", nil, "sh:vpn disconnect")
+  bind("r", nil, "sh:vpn reconnect")
 end)

modules/desktop/hyprland/conf/workspace.lua
@@ -1,40 +1,31 @@
 local h = require("lua.helpers")
+local v = require("lua.vars")
+local d = require("lua.dsp")
 
-h.messaging_clients = {
-  { class = "thunderbird" },
-  { class = "org.telegram.desktop" },
-  { class = "signal" },
-  { class = "vesktop" },
-}
+local bind = h.bind
+local Meta, Control, Shift, Alt = v.mods.Meta, v.mods.Control, v.mods.Shift, v.mods.Alt
 
-local Meta = h.mods.Meta
-local Shift = h.mods.Shift
-local Control = h.mods.Control
-local Alt = h.mods.Alt
+d.dispatchers:add("tmux", function(session_name)
+  local cmd = string.format("tmux new-session -As %q", session_name)
+  return d.cmd("term:" .. cmd, session_name .. "-session")
+end)
 
-local function open_tmux_session_cmd(name)
-  local cmd = string.format("tmux new-session -As %q", name)
-  return h.terminal:cmd(cmd, name .. "-session")
-end
-
-local function drun_cmd(cmd) return h.drun:cmd(cmd) end
 local workspaces = {
-  [1] = { on_created_empty = drun_cmd(h.apps.browser.cmd) },
-  [2] = { on_created_empty = drun_cmd("helium") },
+  [1] = { on_created_empty = v.apps.browser.main },
+  [2] = { on_created_empty = "app:helium" },
   [3] = {
-    on_created_empty = open_tmux_session_cmd("monitors"),
-    clients = { { class = "monitors-session" } }
+    on_created_empty = "tmux:monitors",
+    clients = {
+      { class = "monitors-session" }
+    }
   },
 
   -- named/special
-  workshop = { key = "G", on_created_empty = open_tmux_session_cmd("workshop") },
-  messaging = {
-    key = "D",
-    clients = h.messaging_clients,
-  },
+  workshop = { key = "G", on_created_empty = "tmux:workshop" },
+  messaging = { key = "D", clients = v.messaging_clients, },
   music = {
     key = "S",
-    on_created_empty = drun_cmd("env -u DISPLAY spotify"),
+    on_created_empty = "drun:env -u DISPLAY spotify",
     clients = {
       { class = "^(spotify)$" },
     },
@@ -66,22 +57,23 @@ local function smart_focus(workspace, close_all)
     hl.dispatch(dispatcher)
   end
 end
+
 local function workspace_rules(name, mod, i)
   local workspace = string.gsub(tostring(name), special_prefix, "")
   local opts = workspaces[tonumber(workspace) or workspace]
   local key = (opts and opts.key) or i or workspace
 
-  h.bind(key, mod, smart_focus(name, true))
-  h.bind(key, { mod, Alt }, smart_focus(name, false))
-  h.bind(key, { mod, Shift }, hl.dsp.window.move({ workspace = name, follow = true }))
-  h.bind(key, { mod, Control }, hl.dsp.window.move({ workspace = name, follow = false }))
+  bind(key, mod, smart_focus(name, true))
+  bind(key, { mod, Alt }, smart_focus(name, false))
+  bind(key, { mod, Shift }, hl.dsp.window.move({ workspace = name, follow = true }))
+  bind(key, { mod, Control }, hl.dsp.window.move({ workspace = name, follow = false }))
 
   if not opts then return end
 
   if opts.on_created_empty then
     hl.workspace_rule({
       workspace = tostring(name),
-      on_created_empty = opts.on_created_empty,
+      on_created_empty = d.cmd(opts.on_created_empty),
     })
   end
 
@@ -113,7 +105,7 @@ for name, _ in next, workspaces do
   end
 end
 
-h.bind("T", { Meta, Shift }, function()
+bind("T", { Meta, Shift }, function()
   local layouts = { "dwindle", "master", "scrolling", "monocle" }
 
   local active = h.get_active_or_special_workspace()
@@ -135,10 +127,10 @@ h.bind("T", { Meta, Shift }, function()
   hl.notification.create({ timeout = 2000, text = next_layout })
 end)
 
-h.bind("Tab", Meta, hl.dsp.focus({ workspace = "m+1" }))
-h.bind("Tab", { Meta, Shift }, hl.dsp.focus({ workspace = "m-1" }))
-h.bind("mouse_up", Meta, hl.dsp.focus({ workspace = "m-1" }))
-h.bind("mouse_down", Meta, hl.dsp.focus({ workspace = "m+1" }))
+bind("Tab", Meta, hl.dsp.focus({ workspace = "m+1" }))
+bind("Tab", { Meta, Shift }, hl.dsp.focus({ workspace = "m-1" }))
+bind("mouse_up", Meta, hl.dsp.focus({ workspace = "m-1" }))
+bind("mouse_down", Meta, hl.dsp.focus({ workspace = "m+1" }))
 
 hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
 hl.gesture({ fingers = 3, direction = "vertical", action = "special", workspace_name = "workshop" })